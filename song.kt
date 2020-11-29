d4335b00f342b849b1e1f98f4657726db7136be0
Velocifero1052 <commelevent1052@yandex.ru>
2020-11-29 13:25:51 +0500

    first commit

---
+++ song.kt
@@ -0,0 +1,25 @@
+
+val title: String = "Tunic(Song for Karen)"
+Log.d("Title", title)
+
+val album: String = "Goo"
+Log.d("Album", album)
+
+val artist: String = "Sonic Youth"
+Log.d("Artist", artist)
+
+val year: Int = 1990
+Log.d("Year released", year)
+
+val genre: String = "Alternative rock"
+Log.d("Genre", genre)
+
+val ratingByPitchFork: Float = 4.5f
+Log.d("PitchForkRating", ratingByPitchFork)
+
+val salesInUK: Double = 60.0000
+Log.d("Sales in UK", salesInUK)
+
+
+
+
